6a539d11ca12faf79a6e98dbaedd2bf119b6593e
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-05-28 22:56:18 +0100

    updated subnet

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,15 @@
 # Create a VPC
 resource "aws_vpc" "this" {
   cidr_block = "10.0.0.0/16"
+  tags = {
+    name = "cassandra-vpc"
+  }
 }
 
 
 resource "aws_subnet" "this" {
   vpc_id            = aws_vpc.this.id
-  cidr_block        = "172.16.10.0/24"
+  cidr_block        = "10.10.0/24"
   availability_zone = "eu-west-2a"
 
   tags = {
