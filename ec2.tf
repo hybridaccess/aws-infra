b887f01160cf7e9cf5031511796b880a780a35e8
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-05-29 18:37:50 +0100

    updated ec2 count

--- ec2.tf
+++ ec2.tf
@@ -1,4 +1,5 @@
 resource "aws_network_interface" "this" {
+  count       = 1
   subnet_id   = aws_subnet.this.id
   private_ips = ["192.168.10.10"]
 
@@ -8,11 +9,15 @@ resource "aws_network_interface" "this" {
 }
 
 resource "aws_instance" "this" {
+  count         = 1
   ami           = "ami-0d729d2846a86a9e7" # eu-west-a
   instance_type = "t2.micro"
+  tags = {
+    Name = "cassandra-${count.index}"
+  }
 
   network_interface {
-    network_interface_id = aws_network_interface.this.id
+    network_interface_id = aws_network_interface.this[count.index].id
     device_index         = 0
   }
 }
