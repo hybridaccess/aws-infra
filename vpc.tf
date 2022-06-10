574489daddd291b4adb39453d368320ecc4456af
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-06-10 22:51:04 +0100

    enabled auto-assign public ip on public subnet

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,9 @@
 # Create a VPC
 resource "aws_vpc" "this" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   tags = {
     Name = "cassandra-vpc"
   }
@@ -12,15 +15,17 @@ resource "aws_subnet" "private" {
   cidr_block        = "10.0.0.0/24"
   availability_zone = "eu-west-2a"
 
+
   tags = {
     Name = "private-subnet"
   }
 }
 
 resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "eu-west-2a"
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "public-subnet"
