1fd99bd84c9e5995776b0647659256e930f37271
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-06-10 21:59:08 +0100

    updated vpc subnets

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
+/*
 resource "aws_network_interface" "this" {
   count       = 1
-  subnet_id   = aws_subnet.this.id
+  subnet_id   = aws_subnet.private.id
   private_ips = ["192.168.10.10"]
 
   tags = {
@@ -8,16 +9,28 @@ resource "aws_network_interface" "this" {
   }
 }
 
-resource "aws_instance" "this" {
+resource "aws_instance" "cassandra" {
   count         = 1
-  ami           = "ami-0d729d2846a86a9e7" # eu-west-a
+  ami           = "ami-05ad77e76f6b349d6" # eu-west-a
   instance_type = "t2.micro"
+  availability_zone = ""
+  hibernation = false
+  monitoring = false
+  disable_api_termination = false
   tags = {
     Name = "cassandra-${count.index}"
   }
+
+  root_block_device {
+    delete_on_termination = true
+    volume_type = "gp2"
+    volume_size = ""
+  }
+
 
   network_interface {
     network_interface_id = aws_network_interface.this[count.index].id
     device_index         = 0
   }
 }
+*/
