78256f379fe0bba1e569d15ddbcf132a4b5a96a9
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-06-11 00:00:47 +0100

    update private/public routing

--- vpc.tf
+++ vpc.tf
@@ -1,4 +1,5 @@
-# Create a VPC
+// Create a VPC
+
 resource "aws_vpc" "this" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
@@ -9,11 +10,13 @@ resource "aws_vpc" "this" {
   }
 }
 
+// Subnets
 
 resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "10.0.0.0/24"
-  availability_zone = "eu-west-2a"
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "eu-west-2a"
+  map_public_ip_on_launch = false
 
 
   tags = {
@@ -30,4 +33,61 @@ resource "aws_subnet" "public" {
   tags = {
     Name = "public-subnet"
   }
+}
+
+// Route table and routes
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "private"
+  }
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "public"
+  }
+}
+
+// public routes
+
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+// Route table subnet association
+
+resource "aws_route_table_association" "private" {
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.private.id
+}
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.public.id
+}
+
+
+// Internet Gateway
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+
+  tags = {
+    Name = "egress"
+  }
+}
+
+// NAT Gateway
+
+resource "aws_nat_gateway" "this" {
+  subnet_id = aws_subnet.private.id
+
 }
