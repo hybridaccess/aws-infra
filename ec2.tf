3e28db9be477c4e061df51c536d0d5101ac0af0b
chrisorisawayi <christopher.orisawayi@hybridaccess.net>
2022-06-12 15:18:04 +0100

    added bastion instance

--- ec2.tf
+++ ec2.tf
@@ -34,3 +34,36 @@ resource "aws_instance" "cassandra" {
   }
 }
 */
+
+resource "aws_network_interface" "bastion" {
+  subnet_id = aws_subnet.public.id
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+resource "aws_instance" "bastion" {
+  count                   = 1
+  ami                     = "ami-05ad77e76f6b349d6" # eu-west-a
+  instance_type           = "t2.micro"
+  availability_zone       = "eu-west-2a"
+  hibernation             = false
+  monitoring              = false
+  disable_api_termination = false
+  tags = {
+    Name = "bastion-0${count.index}"
+  }
+
+  root_block_device {
+    delete_on_termination = true
+    volume_type           = "gp2"
+    volume_size           = ""
+  }
+
+
+  network_interface {
+    network_interface_id = aws_network_interface.bastion[count.index].id
+    device_index         = 0
+  }
+}
